f70ffbab12e88dd3216fe91fdb55859d2c4da788
jwong1109 <83558222+jwong1109@users.noreply.github.com>
2023-05-07 18:25:20 +1200

    Create 00_MQ_v1.py

---
+++ 00_MQ_v1.py
@@ -0,0 +1,37 @@
+# Import Statements
+
+# Root Window
+
+# Questions Class
+
+# Functions go here
+
+# Random month generator function
+
+# Easy ask function
+
+# Hard ask function
+
+# Submit answer function
+
+# Feedback function
+
+# Final score function
+
+# Export record of score, questions, and answers to text file function
+
+# ******** Main Routine ********
+
+# Necessary Lists and Variables
+
+# Add Questions to Class
+
+# Labels
+
+# Easy and Hard Button
+
+# Submit answer button
+
+# Finish Quiz
+
+# root.mainloop()
