c5e5d1c79f022eab17ec06636f460dcc7d871824
jwong1109 <83558222+jwong1109@users.noreply.github.com>
2023-05-09 22:21:47 +1200

    Component 1

---
+++ 01_setup_interface_v1.py
@@ -0,0 +1,11 @@
+# Creating Root Window and Background Colour
+
+from tkinter import *
+
+# Root Window
+root = Tk()
+root.title("Te Reo Maori Months Quiz")
+root.geometry("600x700")
+root.configure(bg="red")
+
+root.mainloop()
---
+++ 01_setup_interface_v2_trial1.py
@@ -0,0 +1,12 @@
+# Creating Centre Title Label + Instructions on the left and a few
+# buttons for multiple choice and question labels on the right using pack
+
+from tkinter import *
+
+# Root Window
+root = Tk()
+root.title("Te Reo Maori Months Quiz")
+root.geometry("600x700")
+root.configure(bg="red")
+
+root.mainloop()
