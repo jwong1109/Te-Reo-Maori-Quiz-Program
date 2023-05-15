f5f4aa3634f1af067443558f63eab5aab45c3332
jwong1109 <83558222+jwong1109@users.noreply.github.com>
2023-05-15 12:36:19 +1200

    Component 2

---
+++ 02_setup_questions_v1.py
@@ -0,0 +1,29 @@
+# Creating the months as a class with the Maori name, and English name,
+# and numeric month as objects and putting these details into a list.
+# Create a variable to store the number of questions so far
+# and the number of correct questions.
+
+class Questions:
+    def __init__(self, eng_month, maori_month, numeric_month):
+        self.eng_month = eng_month
+        self.maori_month = maori_month
+        self.numeric_month = numeric_month
+        questions.append(self)
+
+
+questions = []
+num_questions = 0
+num_correct = 0
+
+Questions("January", "Kohi-tātea", 1)
+Questions("February", "Hui-tanguru", 2)
+Questions("March", "Poutū-te-rangi", 3)
+Questions("April", "Paenga-whāwhā", 4)
+Questions("May", "Haratua", 5)
+Questions("June", "Pipiri", 6)
+Questions("July", "Hōngongoi", 7)
+Questions("August", "Here-turi-kōkā", 8)
+Questions("September", "Mahuru", 9)
+Questions("October", "Whiringa-ā-nuku", 10)
+Questions("November", "Whiringa-ā-rangi", 11)
+Questions("December", "Hakihea", 12)
---
+++ 02_setup_questions_v2_trial1.py
@@ -0,0 +1,44 @@
+# Trial 1: Create a random month question generator function using a random
+# number
+
+import random
+
+
+class Questions:
+    def __init__(self, eng_month, maori_month, numeric_month):
+        self.eng_month = eng_month
+        self.maori_month = maori_month
+        self.numeric_month = numeric_month
+        questions.append(self)
+
+
+def random_question_generator():
+    random_num = random.randint(1, 12)
+    for month in questions:
+        if month.numeric_month == random_num:
+            current_question.append(month.eng_month)
+            current_question.append(month.maori_month)
+            current_question.append(month.numeric_month)
+            return current_question
+
+
+current_question = []
+questions = []
+num_questions = 0
+num_correct = 0
+
+Questions("January", "Kohi-tātea", 1)
+Questions("February", "Hui-tanguru", 2)
+Questions("March", "Poutū-te-rangi", 3)
+Questions("April", "Paenga-whāwhā", 4)
+Questions("May", "Haratua", 5)
+Questions("June", "Pipiri", 6)
+Questions("July", "Hōngongoi", 7)
+Questions("August", "Here-turi-kōkā", 8)
+Questions("September", "Mahuru", 9)
+Questions("October", "Whiringa-ā-nuku", 10)
+Questions("November", "Whiringa-ā-rangi", 11)
+Questions("December", "Hakihea", 12)
+
+# For testing purposes
+print(random_question_generator())
